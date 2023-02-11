9103e97f47cef779691caa80e299eac8dda3965b
Adam Skoufis <adam.skoufis@gmail.com>
2023-02-11 22:29:26 +1100

    Add `center-code` layout

---
+++ layouts/center-code.vue
@@ -0,0 +1,20 @@
+<script setup lang="ts">
+const props = defineProps({
+  size: {
+    type: Number,
+    default: 5,
+    // Don't really want to go above ten, it's VERY big and fits only a few lines on screen
+    validator: (value: number) => typeof value === 'number' && value >= 1 && value <= 10
+  }
+})
+
+const codeClass = `code-${props.size}`;
+</script>
+
+<template>
+  <div class="slidev-layout center h-full grid place-content-center">
+    <div class="my-auto" :class="codeClass">
+      <slot />
+    </div>
+  </div>
+</template>
